51c58a79a9cfd6ef9482067c39c793947921ebd1
Gregory <neogen570@gmail.com>
2020-11-21 21:32:25 +0200

    ocaml/snail: working solution

--- ocaml/snail/lib/snail.ml
+++ ocaml/snail/lib/snail.ml
@@ -1,17 +1,24 @@
 (* 
   Algorithm:
   1. Get dimensions of the map
-  2. Get position and end positions for straight line
-  3. Iterate from position to end position collecting path values untile position <> end
+  2. Get start and end positions for straight line
+  3. Iterate from position to end position collecting path values until position <> end
   4. Find next direction
   5. update dimensions
   6. go to 2. until both dimensions <> 0 
 *)
+let nth, length, rev = List.(nth, length, rev)
+
 type direction = East | West | North | South
 
 type position = int * int
 
-type line = position * position
+let direction_to_string dir =
+  match dir with
+  | East -> "East"
+  | West -> "West"
+  | South -> "South"
+  | North -> "North"
 
 let move (x, y) direction =
   match direction with
@@ -20,53 +27,46 @@ let move (x, y) direction =
   | North -> (x, y - 1)
   | South -> (x, y + 1)
 
-let next_direction direction =
-  match direction with
-  | East -> South
-  | South -> West
-  | West -> North
-  | North -> East
+let find_direction (start_x, start_y) (x, y) width height direction =
+  match
+    ( direction
+    , x - start_x = width - 1
+    , y - start_y = height - 1
+    , start_x = x
+    , start_y = y )
+  with
+  | East, true, _, _, _ -> South
+  | South, _, true, _, _ -> West
+  | West, _, _, true, _ -> North
+  | _ -> direction
 
-let find_line direction ((x1, y1), (x2, y2)) width height =
-  match direction with
-  | East -> ((x1, y1), (x2 + width, y2))
-  | South -> ((x2, y2), (x2, y2 + height))
-  | West -> ((x2, y2), (x2 - width, y2))
-  | North -> ((x2, y2), (x2, y2 - height))
+let step map (x, y) = nth (nth map y) x
 
-let update_dimensions width height direction =
-  match direction with
-  | East | West -> (width, height - 1)
-  | South | North -> (width - 1, height)
+let height = length
 
-let should_stop ((x1, y1), (x2, y2)) = x1 = x2 || y1 = y2
+let width map = nth map 0 |> length
 
-let no_dimensions width height = width = 0 || height = 0
-
-let step map (x, y) = List.nth (List.nth map y) x
-
-let height = List.length
-
-let width map = List.nth map 0 |> List.length
-
-let rec collect_line (position, finish) direction map steps =
-  if should_stop (position, finish) then step map position :: steps
+let rec circle_path map width height start_position current_position direction
+    steps =
+  Printf.printf "w:%d, h:%d, x:%d, y:%d -> %s\n" width height
+    (fst current_position) (snd current_position)
+    (direction_to_string direction) ;
+  if current_position = start_position && length steps <> 0 then steps
   else
-    let new_position = move position direction in
-    let step = step map position in
-    collect_line (new_position, finish) direction map (step :: steps)
+    let steps = steps @ [step map current_position] in
+    let next_direction =
+      find_direction start_position current_position width height direction
+    in
+    let next_position = move current_position next_direction in
+    circle_path map width height start_position next_position next_direction
+      steps
 
-let rec path width height direction line map steps =
-  if no_dimensions width height then steps
-  else
-    let new_steps = collect_line line direction map steps in
-    let new_width, new_height = update_dimensions width height direction in
-    let next_direction = next_direction direction in
-    let new_line = find_line next_direction line new_width new_height in
-    path new_width new_height next_direction new_line map (new_steps @ steps)
+let rec path map width height (x, y) steps =
+  match (width, height) with
+  | 0, 0 -> steps
+  | 1, 1 -> steps @ [step map (x, y)]
+  | _ ->
+      let circle_steps = circle_path map width height (x, y) (x, y) East [] in
+      path map (width - 2) (height - 2) (x + 1, y + 1) (steps @ circle_steps)
 
-let snail map =
-  let width = width map in
-  let height = height map in
-  let start_line = find_line East ((0, 0), (0, 0)) width height in
-  path width height East start_line map []
+let snail map = path map (width map) (height map) (0, 0) []
--- ocaml/snail/test/snailTest.ml
+++ ocaml/snail/test/snailTest.ml
@@ -2,8 +2,11 @@ open Alcotest
 open Snail
 
 let cases =
-  [ ([[1; 2; 3]; [4; 5; 6]; [7; 8; 9]], [1; 2; 3; 6; 9; 8; 7; 4; 5])
-  ; ([[1; 2; 3]; [8; 9; 4]; [7; 6; 5]], [1; 2; 3; 4; 5; 6; 7; 8; 9]) ]
+  [ ([[1; 2]; [4; 5]], [1; 2; 5; 4])
+  ; ([[1; 2; 3]; [4; 5; 6]; [7; 8; 9]], [1; 2; 3; 6; 9; 8; 7; 4; 5])
+  ; ([[1; 2; 3]; [8; 9; 4]; [7; 6; 5]], [1; 2; 3; 4; 5; 6; 7; 8; 9])
+  ; ( [[1; 2; 3; 4]; [12; 13; 14; 5]; [11; 16; 15; 6]; [10; 9; 8; 7]]
+    , [1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16] ) ]
 
 let paths =
   let check (input, expected) =
@@ -12,14 +15,9 @@ let paths =
   in
   ("cases", List.map check cases)
 
-let correct_length () =
-  check int "" 9 (length [[1; 2; 3]; [4; 5; 6]; [7; 8; 9]])
-
 let correct_step () =
   check int "" 5 (step [[1; 2; 3]; [4; 5; 6]; [7; 8; 9]] (1, 1))
 
-let suite =
-  [ ("length", [test_case "correct" `Quick correct_length])
-  ; ("step", [test_case "correct" `Quick correct_step]) ]
+let suite = [("step", [test_case "correct" `Quick correct_step]); paths]
 
 let () = run "Snail" suite
