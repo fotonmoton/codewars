72b76ebebab640f25ccb3faed2c3ec2791911a4f
Gregory <neogen570@gmail.com>
2020-11-21 21:51:10 +0200

    ocaml/snail: refactor, handle edge cases

--- ocaml/snail/lib/snail.ml
+++ ocaml/snail/lib/snail.ml
@@ -1,24 +1,21 @@
 (* 
   Algorithm:
   1. Get dimensions of the map
-  2. Get start and end positions for straight line
-  3. Iterate from position to end position collecting path values until position <> end
-  4. Find next direction
-  5. update dimensions
-  6. go to 2. until both dimensions <> 0 
+  2. Get start position
+  3. Make circle path
+    3.1 Start at current postion and take position value
+    3.2 Find next direction
+    3.3 Find next position (make turns when appropriate)
+    3.4 Repat until current position equals start position 
+  4. Reduce width and height by two
+  5. Increase start postion indices by one, "dive" by diagonal
+  6. Go to 3 and repeat until there are no dimensions
 *)
 let nth, length, rev = List.(nth, length, rev)
 
 type direction = East | West | North | South
 
 type position = int * int
-
-let direction_to_string dir =
-  match dir with
-  | East -> "East"
-  | West -> "West"
-  | South -> "South"
-  | North -> "North"
 
 let move (x, y) direction =
   match direction with
@@ -46,27 +43,26 @@ let height = length
 
 let width map = nth map 0 |> length
 
-let rec circle_path map width height start_position current_position direction
-    steps =
-  Printf.printf "w:%d, h:%d, x:%d, y:%d -> %s\n" width height
-    (fst current_position) (snd current_position)
-    (direction_to_string direction) ;
-  if current_position = start_position && length steps <> 0 then steps
+let rec circle map width height start current direction steps =
+  let should_stop = current = start && length steps <> 0 in
+  if should_stop then steps
   else
-    let steps = steps @ [step map current_position] in
-    let next_direction =
-      find_direction start_position current_position width height direction
-    in
-    let next_position = move current_position next_direction in
-    circle_path map width height start_position next_position next_direction
-      steps
+    let steps = steps @ [step map current] in
+    let next_direction = find_direction start current width height direction in
+    let next_position = move current next_direction in
+    circle map width height start next_position next_direction steps
 
 let rec path map width height (x, y) steps =
   match (width, height) with
+  | _, _ when width <> height -> steps
   | 0, 0 -> steps
   | 1, 1 -> steps @ [step map (x, y)]
   | _ ->
-      let circle_steps = circle_path map width height (x, y) (x, y) East [] in
-      path map (width - 2) (height - 2) (x + 1, y + 1) (steps @ circle_steps)
+      let circle_steps = circle map width height (x, y) (x, y) East [] in
+      let new_width = width - 2 in
+      let new_height = height - 2 in
+      let new_position = (x + 1, y + 1) in
+      let new_steps = steps @ circle_steps in
+      path map new_width new_height new_position new_steps
 
 let snail map = path map (width map) (height map) (0, 0) []
